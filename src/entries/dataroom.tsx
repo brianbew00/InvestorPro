9c7f02d01d8e6c11c5e01bd135af16c368e3e569
brianbew00 <45609802+brianbew00@users.noreply.github.com>
2025-07-25 21:28:30 +0000

    Fix: wrap index and dataroom pages in QueryClientProvider

--- src/entries/dataroom.tsx
+++ src/entries/dataroom.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import DataRoomPage from "../pages/dataroom";
+import DataroomPage from "../pages/dataroom";
 import "../index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<DataRoomPage />);
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <DataroomPage />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
--- src/entries/index.tsx
+++ src/entries/index.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import HomePage from "../pages/index";
+import IndexPage from "../pages/index";
 import "../index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<HomePage />);
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <IndexPage />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
