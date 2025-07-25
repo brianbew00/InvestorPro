136a7b1369b91f253da3c90c11bdc50e4703ecfd
brianbew00 <45609802+brianbew00@users.noreply.github.com>
2025-07-25 17:56:18 +0000

    Fix: correct import path for offerings entry

--- src/entries/offerings.tsx
+++ src/entries/offerings.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import OfferingsPage from "../pages/OfferingsPage";
+import OfferingsPage from "../pages/offerings";
 import "../index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<OfferingsPage />);
