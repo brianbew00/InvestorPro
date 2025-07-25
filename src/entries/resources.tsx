f4503a03241fcf2519dd25c94d6420547f15e10d
brianbew00 <45609802+brianbew00@users.noreply.github.com>
2025-07-25 17:57:49 +0000

    Fix: correct import path for resources entry

--- src/entries/resources.tsx
+++ src/entries/resources.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ResourcesPage from "../pages/ResourcesPage";
+import ResourcesPage from "../pages/resources";
 import "../index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<ResourcesPage />);
